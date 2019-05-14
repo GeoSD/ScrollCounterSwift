f03251aff9f944f16b6d2c0b1d24b5d2f945b541
Georgy Dyagilev <g.dyagilev@gmail.com>
2019-05-14 15:49:13 +0700

    Init commit to dev

---
+++ ScrollCounterSwift/ScrollCounterSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollCounterSwift
+//
+//  Created by Georgy Dyagilev on 14/05/2019.
+//  Copyright © 2019 Georgy Dyagilev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
